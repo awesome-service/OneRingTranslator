2e772f6caa30619bf7ffa2a3023728784dd1f28e
Sila Rim <51617383+rimsila@users.noreply.github.com>
2024-07-20 22:05:18 +0700

    update install

--- update.js
+++ update.js
@@ -1,14 +1,21 @@
+const APP_NAME = "OneRingTranslator";
+const ENV_PATH = `${APP_NAME}/env`;
+
 module.exports = {
-  run: [{
-    method: "shell.run",
-    params: {
-      message: "git pull"
-    }
-  }, {
-    method: "shell.run",
-    params: {
-      path: "app",
-      message: "git pull"
-    }
-  }]
-}
+  run: [
+    {
+      method: "shell.run",
+      params: {
+        venv: ENV_PATH, // Edit this to customize the venv folder path
+        path: APP_NAME,
+        message: ["git pull"],
+      },
+    },
+    {
+      method: "notify",
+      params: {
+        html: `Click the 'start ${APP_NAME}' tab to get started!`,
+      },
+    },
+  ],
+};
