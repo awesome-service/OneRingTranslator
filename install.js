99e92c1318bcad9964c2a8368f3ec4ee123bc8da
Sila Rim <51617383+rimsila@users.noreply.github.com>
2024-07-20 21:36:38 +0700

    up

--- install.js
+++ install.js
@@ -1,14 +1,13 @@
 const APP_NAME = "OneRingTranslator";
-const ENV_PATH = `app/${APP_NAME}/env`;
+const ENV_PATH = `${APP_NAME}/env`;
 
 module.exports = {
   run: [
-    // Edit this step with your custom install commands
     {
       method: "shell.run",
       params: {
         message: [
-          `cp -r ${APP_NAME} app/${APP_NAME}`,
+          "git clone --recurse-submodules https://github.com/awesome-service/OneRingTranslator",
         ],
       },
     },
@@ -16,25 +15,24 @@ module.exports = {
       method: "shell.run",
       params: {
         venv: ENV_PATH, // Edit this to customize the venv folder path
-        path: `app/${APP_NAME}`, 
-        message: [
-          "pip install -r requirements.txt"],
+        path: `app/${APP_NAME}`,
+        message: ["pip install -r requirements.txt"],
       },
     },
     {
       method: "fs.link",
       params: {
-        path: `app/${APP_NAME}`, 
+        path: `app/${APP_NAME}`,
       },
     },
-    
+
     //  Uncomment this step to add automatic venv deduplication (Experimental)
-     {
-       method: "fs.link",
-       params: {
-         venv: ENV_PATH,
-       }
-     },
+    {
+      method: "fs.link",
+      params: {
+        venv: ENV_PATH,
+      },
+    },
     {
       method: "notify",
       params: {
@@ -43,4 +41,3 @@ module.exports = {
     },
   ],
 };
-
