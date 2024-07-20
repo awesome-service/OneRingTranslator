4912d4c244e846bb265a08aee675ef177da83e0c
Sila Infinity <sila083.infinity@gmail.com>
2024-07-20 15:24:03 +0700

    update

--- install.js
+++ install.js
@@ -1,34 +1,46 @@
+const APP_NAME = "OneRingTranslator";
+const ENV_PATH = `app/${APP_NAME}/env`;
+
 module.exports = {
   run: [
-    // Edit this step to customize the git repository to use
-    {
-      method: "shell.run",
-      params: {
-        message: [
-          "git clone https://github.com/awesome-service/OneRingTranslator app",
-        ],
-      },
-    },
     // Edit this step with your custom install commands
     {
       method: "shell.run",
       params: {
-        venv: "env", // Edit this to customize the venv folder path
-        path: "app", // Edit this to customize the path to start the shell from
-        message: ["pip install -r requirements.txt"],
+        message: [
+          `cp -r ${APP_NAME} app/${APP_NAME}`,
+        ],
+      },
+    },
+    {
+      method: "shell.run",
+      params: {
+        venv: ENV_PATH, // Edit this to customize the venv folder path
+        path: `app/${APP_NAME}`, 
+        message: [
+          "pip install -r requirements.txt"],
       },
     },
     {
       method: "fs.link",
       params: {
-        venv: "app/env",
+        path: `app/${APP_NAME}`, 
       },
     },
+    
+    //  Uncomment this step to add automatic venv deduplication (Experimental)
+     {
+       method: "fs.link",
+       params: {
+         venv: ENV_PATH,
+       }
+     },
     {
       method: "notify",
       params: {
-        html: "Click the 'start' tab to get started!",
+        html: `Click the 'start ${APP_NAME}' tab to get started!`,
       },
     },
   ],
 };
+
